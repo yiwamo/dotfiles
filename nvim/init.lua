6f9998fc92d254fa3c5e1f3e100356f773ec1854
Yasuhiro Iwamoto <yiwamo-ms@ValueUpConsultingnoMac-Studio.local>
2026-01-19 14:06:02 +0900

    add lazygit

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- チートシートを開く
+
+vim.api.nvim_create_user_command("Opencheatsheet", function()
+  vim.cmd("edit ~/.dotmemo/nvim/CHEATSHEET.md")
+end, {})
